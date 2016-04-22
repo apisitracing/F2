6ad9edbd06a038c8921e7d3f3496a15ead0e6d54
apisitracing <apisitracing@hotmail.com>
2016-04-22 19:00:21 +0700

    Added files via upload

---
+++ Bullet.java
@@ -0,0 +1,47 @@
+package f2.spw;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
+public class Bullet extends Sprite{
+	public static final int Y_TO_FADE = 20; //distance of bullet that start to fade
+	public static final int Y_TO_DIE = 0; //end of panel, when bullet out of panel it's die
+	
+	private int step = 20; //speed of bullet
+	private boolean alive = true;
+	
+	public Bullet(int x, int y) {
+		super(x , y, 5, 15);	
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		if(y > Y_TO_FADE)
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		else{
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
+					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
+		}
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, width, height);
+		
+	}
+
+	public void proceed(){
+		y -= step;
+		if(y < Y_TO_DIE){
+			alive = false;
+		}
+	}
+	
+	public boolean isAlive(){
+		return alive;
+	}
+	
+	public void getHit(){
+		this.alive = false;
+	}
+
+}
---
+++ GamePanel.java
@@ -0,0 +1,49 @@
+package f2.spw;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage bi;	
+	Graphics2D big;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+
+	public GamePanel() {
+		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		big = (Graphics2D) bi.getGraphics();
+		big.setBackground(Color.BLACK);
+	}
+
+	public void updateGameUI(GameReporter reporter){
+		big.clearRect(0, 0, 400, 600);
+		
+		big.setColor(Color.WHITE);
+		big.drawString(String.format("Score %08d", reporter.getScore()), 260, 20);
+		big.drawString(String.format("HP : %03d", reporter.getHp()), 150, 20);
+		big.drawString(String.format("Life Point : %01d", reporter.getLp()), 20, 20);
+		for(Sprite s : sprites){
+			s.draw(big);
+		}
+		repaint();
+		
+		
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		int s0 = 0;
+		g2d.drawImage(bi, null, s0, 0);
+	}
+
+}
